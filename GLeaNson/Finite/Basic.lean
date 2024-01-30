b43f12dfaeb133fde9ea7f30374fe30ef074394d
samvang <59202064+samvang@users.noreply.github.com>
2024-01-30 12:17:54 +0100

    some comments after readig more of mathlib

--- GLeaNson/Finite/Basic.lean
+++ GLeaNson/Finite/Basic.lean
@@ -15,3 +15,35 @@ example : PartialOrder {a : α // SupIrred a} := by exact Subtype.partialOrder f
 -- TODO Perhaps find a way to use `birkhoofFinset` rather than `birkhooSet`
 lemma InImageBirkhoffIff (u : (Set ({a : α // SupIrred a}))) :
   u ∈ range (birkhoffSet α) ↔ IsLowerSet u := by sorry
+
+/- (in this proof blueprint below, just for readability,
+I write f for birkhoffSet and J α for {a : α // SupIrred a} )
+first suppose u = f a for some a : α.
+we show that f a is a lower set.
+suppose x ∈ f a and y : J α  with y ≤ x.
+by def of f a, x ≤ a.
+now y ≤ a by transitivity.
+thus,  y ∈ f a as required.
+
+conversely, suppose u is a lower set.
+we define a := sup u, and we claim that f(sup u) = u.
+let x : J α. We have:
+x ∈ f (sup u) ↔ x ≤ sup u (definition of f)
+↔ x ≤ y for some y ∈ u (lemma about join-prime in distributive lattices)
+↔ x ∈ u (since u is a lower set)
+-/
+
+/- Actually, I just realized that the above proof is already in mathlib under `OrderIso.lowerSetSupIrred`. -/
+
+
+/- There is also
+"the other unitor of Birkhoff's representation theorem"
+`noncomputable def OrderIso.supIrredLowerSet`
+but it only applies to α which is DistribLattice, and this is not general enough.
+In order to establish the dual equivalence between finite DL's and finite posets, what we will
+need is the following statement, which is strictly more general: -/
+
+variable (β : Type)
+variable [PartialOrder β] [Fintype β]
+noncomputable def OrderIso.supIrredLowerSet' :
+  β ≃o {s : LowerSet β // SupIrred s} := sorry
